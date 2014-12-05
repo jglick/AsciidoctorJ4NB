15b9e70e0d7b3d65f3548707a42a087df50fce8b
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2014-12-05 06:24:25 +0100

    asciidoc file support, icon, and file template

--- AsciidoctorJ4NB/src/org/netbeans/asciidoc/package-info.java
+++ AsciidoctorJ4NB/src/org/netbeans/asciidoc/package-info.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-@TemplateRegistration(folder = "Other", content = "AdocTemplate.adoc")
+@TemplateRegistration(folder = "Other", displayName = "AsciiDoc", content = "AsciiDoc.adoc", requireProject = false)
 package org.netbeans.asciidoc;
 
 import org.netbeans.api.templates.TemplateRegistration;
