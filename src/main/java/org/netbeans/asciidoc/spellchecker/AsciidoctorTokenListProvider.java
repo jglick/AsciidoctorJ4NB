b7fc9b4e7bc9e0911198dbeb462b93e4d6ffdae8
Attila Kelemen <attila.kelemen85@gmail.com>
2016-12-11 20:55:17 +0100

    Added spellchecking support (copied from the Markdow plugin).

---
+++ src/main/java/org/netbeans/asciidoc/spellchecker/AsciidoctorTokenListProvider.java
@@ -0,0 +1,35 @@
+package org.netbeans.asciidoc.spellchecker;
+
+import javax.swing.text.Document;
+import org.netbeans.api.editor.mimelookup.MimeRegistration;
+import org.netbeans.asciidoc.highlighter.AsciidoctorLanguageConfig;
+import org.netbeans.editor.BaseDocument;
+import org.netbeans.modules.spellchecker.spi.language.TokenList;
+import org.netbeans.modules.spellchecker.spi.language.TokenListProvider;
+
+/**
+ * Copied (adjusted) from the Markdown plugin
+ *
+ * @author Holger Stenger
+ */
+@MimeRegistration(
+        mimeType = AsciidoctorLanguageConfig.MIME_TYPE,
+        service = TokenListProvider.class,
+        position = 1010)
+public class AsciidoctorTokenListProvider implements TokenListProvider {
+
+    public AsciidoctorTokenListProvider() {
+    }
+
+    @Override
+    public TokenList findTokenList(Document doc) {
+        if (doc instanceof BaseDocument) {
+            BaseDocument baseDoc = (BaseDocument)doc;
+            final Object mimeType = baseDoc.getProperty("mimeType");
+            if (AsciidoctorLanguageConfig.MIME_TYPE.equals(mimeType)) {
+                return new AsciidoctorTokenList(doc);
+            }
+        }
+        return null;
+    }
+}
